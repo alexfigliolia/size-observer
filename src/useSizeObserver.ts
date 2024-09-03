e6d584400e2594312abc8dde2e59cf5abd7dff4b
Alex Figliolia <alexfigliolia@gmail.com>
2024-09-03 12:59:12 -0400

    Stablize options

--- src/useSizeObserver.ts
+++ src/useSizeObserver.ts
@@ -4,14 +4,20 @@ import type { Options } from "./types";
 
 export const useSizeObserver = <T extends Element>(options: Options) => {
   const node = useRef<T>(null);
+  const stableOptions = useRef<Options>(options);
+  stableOptions.current = options;
+  const observer = useRef<SizeObserver<T> | null>(null);
+  if (observer.current) {
+    observer.current.options = stableOptions.current;
+  }
   useLayoutEffect(() => {
     if (!node.current) {
       return;
     }
-    const observer = new SizeObserver(node.current, options);
+    observer.current = new SizeObserver(node.current, stableOptions.current);
     return () => {
-      observer.destroy();
+      observer.current?.destroy();
     };
-  }, [options]);
+  }, []);
   return node;
 };
